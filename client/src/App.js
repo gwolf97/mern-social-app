a6e7e898f9ca3f16fa996f316bc23c426bc56c39
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-08-05 16:09:43 -0500

    Client and server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+    <AppBar position="static" color="inherit">
+      <Typography variant="h2" align="center">mern-social-app</Typography>
+    </AppBar>
+    <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <h1>posts</h1>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <h1>form</h1>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+  </Container>
   );
 }
 
