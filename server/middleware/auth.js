baeefda476c0457bb4b9b89e88d3f9f9c2355479
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-08-24 17:38:55 -0500

    Post comments model and auth middleware fix

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ import asyncHandler from "express-async-handler"
 
 export const auth = asyncHandler(async (req, res, next) =>{
     try {
-        const token = req.body.headers.Authorization.split(" ")[1]
+        const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : req.body.headers.Authorization.split(" ")[1]
         const isCustomAuth = token.length < 500
         
         let decodedData
--- server/models/postModal.js
+++ server/models/postModal.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose"
 
+const commentSchema = mongoose.Schema({
+    name:{type:String, required:true},
+    comment:{type:String, required:true},
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+},{
+    timestamps:true
+})
+
 const postSchema = mongoose.Schema({
     message: String,
     name: String,
@@ -10,6 +22,7 @@ const postSchema = mongoose.Schema({
         type: [String],
         dfault: [],
     },
+    comments:[commentSchema],
     createdAt:{
         type: Date,
         default: new Date(),
