d52a0920124ab46e43595e1511dfbb385a279dff
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-08-24 17:58:17 -0500

    Redux authdata bug fix

--- client/src/store.js
+++ client/src/store.js
@@ -14,7 +14,7 @@ const reducer = combineReducers({
 })
 
 const initialState = {
-    auth: JSON.parse(localStorage.getItem("profile")) || null
+    auth: {authData: JSON.parse(localStorage.getItem("profile"))} || {authData: null}
 }
 
 const middleware = [thunk]
