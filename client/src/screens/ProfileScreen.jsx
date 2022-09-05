56a256531fb3358aba1a34e99915ac40815a8194
Gabriel Wolf <92558787+gwolf97@users.noreply.github.com>
2022-09-05 14:30:14 -0500

    Image uploader on profile page

--- client/src/screens/ProfileScreen.jsx
+++ client/src/screens/ProfileScreen.jsx
@@ -1,15 +1,58 @@
 import React from 'react'
+import axios from "axios"
 import { useSelector } from 'react-redux'
+import {Avatar} from '@mui/material';
 
 const ProfileScreen = () => {
 
 const user = useSelector(state => state.auth.authData)
 
+const [uploading, setUploading] = React.useState(false)
+const [profileData, setProfileData] = React.useState({file:"../images/profileimg.jpg"})
+
+const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const formData = new FormData()
+    formData.append("image", file)
+    setUploading(true)
+    
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        }
+
+        const {data} = await axios.post("http://localhost:5000/upload", formData, config)
+
+        setProfileData({file: data})
+        setUploading(false)
+    } catch (error) {
+        console.error(error)
+        setUploading(false)
+        setProfileData({file:""})
+    }
+  }
+
+  console.log(profileData)
+
   return (
+    <>
     <section style={{width:"100vw", height:"80vh", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
-    <img style={{width:"300px", borderRadius:"50%"}} src="../images/profileimg.jpg"/> 
-    <h3 style={{color:"#fefefe", marginTop:"40px"}} >{user.result.name.toUpperCase()}</h3>
+    <Avatar sx={{width:300, height:300}} src={profileData.file} variant="contained"/>
+        <h3 style={{color:"#fefefe", marginTop:"40px"}} >{user.result.name.toUpperCase()}</h3>
     </section>
+    <section>
+         <div className="image-upload">
+            <label htmlFor="file-input">
+              <i style={{cursor:"pointer", color:"#408BF7", fontSize:"12px", marginRight:"10px"}} className="fa-solid fa-image"> change profile picture</i>
+            </label>
+            <input id="file-input" type="file" multiple={false} onChange={uploadFileHandler} />
+        </div>
+    </section>
+    </>
+
+
 
   )
 }
